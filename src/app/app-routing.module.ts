9679aeb8a5ab740c27e632d09fe0a0b40eca248b
Daniel Gutierrez Muñoz <dagutmu667@gmail.com>
2022-06-03 20:19:00 -0500

    Class 176: Child routes and Lazy Loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,11 @@ import { ErrorPageComponent } from './shared/error-page/error-page.component';
 
 const routes: Routes = [
   {
+    path: 'auth',
+    loadChildren: () =>
+      import('./auth/auth.module').then((module) => module.AuthModule),
+  },
+  {
     path: '404',
     component: ErrorPageComponent,
   },
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,16 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
-
-
+import { AuthRoutingModule } from './auth-routing.module';
 
 @NgModule({
-  declarations: [
-    LoginPageComponent,
-    RegisterPageComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [LoginPageComponent, RegisterPageComponent],
+  imports: [CommonModule, AuthRoutingModule],
 })
-export class AuthModule { }
+export class AuthModule {}
